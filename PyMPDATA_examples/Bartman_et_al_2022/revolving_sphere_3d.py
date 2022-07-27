de7f7f5e8856ed5cfbf70c0a04eed215eb8fc138
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-07-27 15:42:25 +0200

    addressing pylint suggestions

--- PyMPDATA_examples/Bartman_et_al_2022/revolving_sphere_3d.py
+++ PyMPDATA_examples/Bartman_et_al_2022/revolving_sphere_3d.py
@@ -1,4 +1,3 @@
-import time
 import timeit
 import json
 import os
@@ -11,7 +10,7 @@ def run_simulation(*, static, n_threads, nx, dt, nt):
     settings = Settings(n=nx, dt=dt)
     options = Options(n_iters=int(os.environ['N_ITERS']))
     simulation = Simulation(settings, options, static=static, n_threads=n_threads)
-    cpu_time_per_timestep = simulation.run(nt=nt)
+    _ = simulation.run(nt=nt)
 
 def time_pympdata(*, static, n_threads, nx, dt, nt, repeat):
     cmd = f"run_simulation(static={static}, n_threads={n_threads}, nx={nx}, dt={dt}, nt={nt})"
@@ -30,7 +29,9 @@ def time_pympdata(*, static, n_threads, nx, dt, nt, repeat):
     return (time_full - time_init) / (nt - 1)
 
 if __name__ == '__main__':
-    ticks = range(1, int(os.environ['MAX_THREADS'])+1) if os.environ["NUMBA_DISABLE_JIT"] != '1' else (1,)
+    ticks = range(1, int(os.environ['MAX_THREADS'])+1) \
+        if os.environ["NUMBA_DISABLE_JIT"] != '1' \
+        else (1,)
 
     if os.environ["NUMBA_DISABLE_JIT"] == '1':
         with np.errstate(all="ignore"):
@@ -47,18 +48,18 @@ if __name__ == '__main__':
             'static': {i:-1 for i in ticks},
             'dynamic': {i:-1 for i in ticks},
         }
-        for n_threads in ticks:
-            numba.set_num_threads(n_threads)
-            print(f'n_threads={n_threads}')
+        for _n_threads in ticks:
+            numba.set_num_threads(_n_threads)
+            print(f'n_threads={_n_threads}')
             for grid, flag in {'static': True, 'dynamic': False}.items():
-                data[grid][n_threads] = time_pympdata(
+                data[grid][_n_threads] = time_pympdata(
                     static=flag,
-                    n_threads=n_threads,
+                    n_threads=_n_threads,
                     nx=int(os.environ['NX']),
                     dt=float(os.environ['DT']),
                     nt=int(os.environ['N_STEPS']),
                     repeat=int(os.environ['N_REPEATS'])
                 )
 
-    with open(f'data_NDJ={os.environ["NUMBA_DISABLE_JIT"]}.json', 'w') as file:
+    with open(f'data_NDJ={os.environ["NUMBA_DISABLE_JIT"]}.json', 'w', encoding="utf-8") as file:
         json.dump(data, file)
